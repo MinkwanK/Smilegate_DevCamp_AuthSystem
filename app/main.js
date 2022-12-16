6d14a96396a15f9181463372f2a2406847757c5c
MinKwan Kim <minkwan_k@naver.com>
2022-12-17 03:11:37 +0900

    front js

--- app/main.js
+++ app/main.js
@@ -4,16 +4,20 @@ const app = express();
 const PORT = 8080;
 //const ip = require('ip');
 //라우팅
-const home = require("./routes/home")
+const home = require("./src/routes/home")
 
 //앱 세팅, 화면 뷰들을 넘겨줄 파일이 있는 경로를 두번째 파라미터에 넣어준다. <뷰 분리>
-app.set("views","./views");
+app.set("views","./src/views");
 //html 코드를 어떤 엔진으로 해석할지? ejs는 자주 사용하는 엔진 <라우터 분리>
 app.set("view engine","ejs");
 //index.js 로 이동해서 라우터의 코드에 따라 행동하는 형식 
 app.use("/",home); //use -> 미들웨어 등록해주는 메서드
+//정적 경로로 추가 js 스크립트 연결용 미들웨어
+app.use(express.static(`${__dirname}/src/public`)); 
 
 module.exports = app;
+
+
 
 
 
