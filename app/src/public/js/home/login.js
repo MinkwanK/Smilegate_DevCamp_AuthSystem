50667cd317e70d165153af8719a885c576f38032
MinKwan Kim <minkwan_k@naver.com>
2022-12-18 16:27:42 +0900

    register js

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -2,7 +2,7 @@
 
 const id = document.querySelector("#id");
 const pw = document.querySelector("#pw");
-const loginBtn = document.querySelector("#loginbtn");
+const loginBtn = document.querySelector("#button");
 
 loginBtn.addEventListener("click",login);
 
---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const id = document.querySelector("#id");
+const name =document.querySelector("#name");
+const email = document.querySelector("#email")
+const pw = document.querySelector("#pw");
+const confirmPw = document.querySelector("#confirm-pw")
+const registbtn = document.querySelector("#button");
+
+registbtn.addEventListener("click",register);
+
+function register(){
+    const req = {
+        id : id.value,
+        name : name.value,
+        email : email.value,
+        pw : pw.value,
+        confirmPw : confirmPw.value,
+    };
+
+    
+    //fetch함수: 첫번째 인자로 URL, 두번째 옵션객체Promise 타입의 객체 반환. 
+    //반환된 객체는 API 호출이 성공했을 경우에만 응답 객체를 resolve하고 실패하면 예외 객체를 resolve
+    fetch("/register",{ //bodyparser 덕분에 문자열로 변환된 bodydata가 다시 객체화되서 백엔드로 넘겨짐
+        method: "POST",
+        headers:{
+            "Content-Type":"application/json",
+        },
+        body: JSON.stringify(req),
+    })
+        .then((res) => res.json()) //promise 객체 반환
+        .then((res)=>{ 
+        if (res.success) {
+            location.href = "/login";
+        }
+             else{
+                alert(res.msg);
+             }        
+    })
+    .catch((err)=>{
+        console.error("회원가입 중 에러 발생!");
+    });
+}
+
